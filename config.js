c3c6c2a059df18eaee1352735c5d42c31418b615
Lê Minh Khánh <23560056@ms.uit.edu.vn>
2023-11-07 20:31:18 +0700

    first commit

---
+++ config.js
@@ -0,0 +1,17 @@
+const CONFIG = {
+    titleWeb: "Lạy chúa cứu dỗi Chi",
+    introTitle: 'Hãy tha thứ',
+    introDesc: `18 tuổi đầu rồi, chơi mà dỗi!!`,
+    btnIntro: 'Grrrr',
+    title: 'Có tha không?',
+    desc: 'Sự thật chỉ có một!',
+    btnYes: 'Có',
+    btnNo: 'Đéo tha!',
+    question: 'quả là một lựa chọn sáng suốt! đáng khen.',
+    btnReply: 'Gửi câu trả lời',
+    reply: 'Thật ra tớ cũng là chó gâu gâu gâu gâu gâu',
+    mess: 'Ê gửi qua mess á :)) đừng gửi trong này',
+    messDesc: 'Chó không được dỗi',
+    btnAccept: 'Nhấn tiếp đi nhìn gì ',
+    messLink: 'https://www.facebook.com/messages/t/100040564341434'
+}
